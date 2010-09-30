07e92511e220a6be0e50fe3e4ad2d389c158d6aa
Matt Hoffman <mattmhoffman@gmail.com>
2010-09-30 15:06:57 -0400

    updated .rspec to include --drb

--- config/application.rb
+++ config/application.rb
@@ -32,11 +32,21 @@ module SampleApp
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    
+ 
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-  end
-end
+        ### Part of a Spork hack. See http://bit.ly/arY19y
+        if Rails.env.test?
+          initializer :after => :initialize_dependency_mechanism do
+            # Work around initializer in railties/lib/rails/application/bootstrap.rb
+            ActiveSupport::Dependencies.mechanism = :load
+          end
+        end
+      end
+    end    
+    
