7782cf6fff0d801635e36e6f6f6cc9cbdffbe421
Matt Hoffman <mattmhoffman@gmail.com>
2010-09-30 14:31:35 -0400

    Screwed up. Starting over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '12b0f2c40fc2a6b56d5b1078b3ec999b7c7dc9372e4796250d07fef4e9e72b280d80b3907be63ad706207cbfc9df5b9a2cb0c129b250d89f11cf95c0b9080cb4'
